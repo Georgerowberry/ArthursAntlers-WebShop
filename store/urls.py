74787f0f4a29d21956d9231f7efa816fcf111c01
Georgerowberry <george.rowberry@gmail.com>
2017-09-16 12:26:01 +0100

    staticfiles

--- store/urls.py
+++ store/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 urlpatterns = [
     url(r'^$', views.index, name='index'),
 
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
--- storefront/settings.py
+++ storefront/settings.py
@@ -132,11 +132,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = os.path.join(os.path.abspath(
-    os.path.join(BASE_DIR, 'storefront', 'static')), '')
+STATIC_URL = '/staticfiles/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static/"),
-]
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
